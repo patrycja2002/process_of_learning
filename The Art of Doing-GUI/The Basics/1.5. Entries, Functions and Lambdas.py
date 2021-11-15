bd7e35ee2a663e2e44cf98e98c873582226b0c1c
patrycja2002 <80715862+patrycja2002@users.noreply.github.com>
2021-11-15 21:45:50 +0100

    Add files via upload

---
+++ The Art of Doing-GUI/The Basics/1.5. Entries, Functions and Lambdas.py
@@ -0,0 +1,54 @@
+# Entries, Functions and Lambda
+import tkinter
+from tkinter import END
+
+# Define window
+root = tkinter.Tk()
+root.title("Entry Basics!")
+root.geometry("500x500")
+root.resizable(0, 0)
+
+
+# Define Functions
+def make_label():
+    ''''Print a label to the app'''
+    text = tkinter.Label(output_frame, text=text_entry.get(), bg="#cc6666")
+    text.pack()
+
+    text_entry.delete(0, END)
+
+
+def count_up(number):
+    '''Count up on the app'''
+    global value
+
+    text = tkinter.Label(output_frame, text=number, bg="#cc6666")
+    text.pack()
+
+    value = number + 1
+
+# Define frames
+input_frame = tkinter.Frame(root, bg="#ffcccc", width=500, height=100)
+output_frame = tkinter.Frame(root, bg="#cc6666", width=500, height=400)
+input_frame.pack(padx=10, pady=10)
+output_frame.pack(padx=10, pady=(0, 10))
+
+# Add inputs
+text_entry = tkinter.Entry(input_frame, width=40)
+text_entry.grid(row=0, column=0, padx=5, pady=5)
+input_frame.grid_propagate(0)
+
+print_button = tkinter.Button(input_frame, text="Print!", command=make_label)
+print_button.grid(row=0, column=1, padx=5, pady=5, ipadx=30)
+
+# Keep output frame size
+output_frame.pack_propagate(0)
+
+# Pass a parameter with lambda
+value = 0
+count_button = tkinter.Button(input_frame, text="Count!", command=lambda: count_up(value))
+count_button.grid(row=1, column=0, columnspan=2, pady=5, sticky="WE")
+
+
+# Call root window's main loop
+root.mainloop()
